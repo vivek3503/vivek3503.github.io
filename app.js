5552d0889dc37af7538ac66f143358fbc78c4246
vivek suthar <111764621+vivek3503@users.noreply.github.com>
2023-05-01 09:21:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('mobile-menu')
+const menulinks =document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+   menu.classList.toggle('is-active');
+   menulinks.classList.toggle('active'); 
+});
+
+
+ 
